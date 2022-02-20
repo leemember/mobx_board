ef9b7e3d9b55609446b0b8caa64993b03e5911b9
이현주 <hyunmail521@gmail.com>
2022-02-20 16:12:38 +0900

    mobx로observer, observarble 등등 데코레이터 사용

--- mobx_board/README.md
+++ mobx_board/README.md
@@ -20,3 +20,13 @@ $npx json-server ./data.json --port 4000
 ```
 $npx i axios
 ```
+
+## mobx 주요 특징
+
+- 데코레이터를 적극 활용한다. ( 스토어 객체에 붙이는 데코레이터가 있고`@observable`, 컴포넌트에서 사용하는 데코레이터가 있다. `@observar`)
+- ts가 base인 라이브러리이다. ( 당연히 @types/mobx, @types/mobx-react 가 필요 없다. )
+- 리덕스와 다르게 단일 스토어를 강제하진 않는다. (= 리덕스보다는 자유로운 편이다.)
+- 처음 사용이 리덕스보다 훨씬 쉽다. 데코레이터만 달아주면 바로 변화가 온다.
+- 직관적이다.
+- tsconfig.json에 ` "experimentalDecorators": true,`를 추가해준다.
+- 라이프사이클에 대한 고민 !
--- mobx_board/src/App.tsx
+++ mobx_board/src/App.tsx
@@ -1,7 +1,18 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
+import store from "./store";
 
-function App() {
-  return <div className="App">gdgd</div>;
-}
+// @observer는 엇 바꼈네 하고 감지하는 것.
+// function 형은 observer를 이런식으로 @ 없이 사용해준다.
+const App = observer(() => {
+  return (
+    <div className="App">
+      {store.age}
+      <button type="button" onClick={() => store.addAge()}>
+        한 해가 지나갔다.
+      </button>
+    </div>
+  );
+});
 
 export default App;
